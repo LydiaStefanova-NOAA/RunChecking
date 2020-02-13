05e45ba664d9c197ea3e5573389154d4173aed25
Lydia Stefanova <lydia.b.stefanova@noaa.gov>
2020-02-13 19:25:33 +0000

    Add get_b31.sh

---
+++ get_b31.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+#SBATCH --ntasks=1 -p service 
+#SBATCH -t 12:00:00 
+#SBATCH -A fv3-cpu
+#SBATCH -q batch 
+#SBATCH -J b31-getfiles
+
+module load hpss
+rundir=/scratch1/NCEPDEV/stmp2/Lydia.B.Stefanova/fromHPSS/ufs_b31
+
+
+for yyyy in {2011..2018..1} ; do
+   for mm in {1..12..1} ; do
+       mm=$(printf "%02d" $mm)
+       for dd in {1..15..14} ; do
+           dd=$(printf "%02d" $dd)
+           tag=$yyyy$mm${dd}00
+              if [ ! -d $rundir ] ; then mkdir $rundir ; fi
+
+              if [ ! -f $rundir/$tag/gfs.$yyyy$mm$dd/00/gfs.t00z.flux.1p00.f840 ] ; then
+
+              cd $rundir
+              echo working on $tag
+               base=/NCEPDEV/emc-climate/5year/Jiande.Wang/WCOSS/benchmark3.1/c384/$tag
+
+
+
+              archive1=$base/gfs_flux_1p00.tar
+              #archive2=$base/SST.tar
+              #archive2=$base/gfs_pgrb2b.tar
+              #archive3=$base/gfs_flux.tar
+              ocean=$base/ocn.tar
+              ice=$base/ice.tar
+
+              if [ ! -d $tag ] ; then
+                mkdir $tag
+              fi
+
+              cd $tag
+              htar -xvf $archive1
+              #htar -xvf $ice
+   
+           else 
+              echo "skipping $tag"
+           fi
+       done
+   done
+done
